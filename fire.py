af2f0f5951f9231fcb885dd7cd4004e8e9077064
Christian Basse <christianmbasse@gmail.com>
2016-01-31 23:09:38 +1100

    Updated fire code

--- fire.py
+++ fire.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-# global temperature array
+# globals needed for heatmap simulations
+
+# global temparature array
 # holds the current temperature for each of the grid cells
 TEMP_ARR = np.array([])
 
@@ -16,10 +18,22 @@ FUEL_ARR = np.array([])
 def R(T, V):
     if T < 572:
         return 0
-    if V <= 0:
+    if V < 0:
         return 0
     else:
         return 1
+
+
+# cooling - returns the Newton cooling term for dVdt
+# temp - float - current temperature
+# ambientTemp - float - ambient air temperature
+# k - float - Newton cooling rate
+# returns - float - current cooling amount
+def cooling(temp, ambientTemp, k):
+    if temp > ambientTemp:
+        return -k * temp
+    else:
+        return 0
 
 
 # dTdt - performs the differential equation for temperature change in a cell
@@ -28,22 +42,21 @@ def R(T, V):
 # k - float - cooling constant
 # delta - float - combustion constant
 # h - float - height/width of the cell, set to 1 by default
-def dTdt(temp_arr, fuel_arr, kappa, k, delta, h=1):
+# returns - np.array - 2D array of derivatives
+def dTdt(temp_arr, fuel_arr, kappa, k, delta, ambientTemp, h=1):
     # pad the outside of the array with
     temp_arr = np.pad(temp_arr, (1, 1), 'edge')
-    # fuel_arr = np.pad(temp_arr, (1, 1), 'constant')
     new_arr = []
     rows = range(1, len(temp_arr) - 1)
     cols = range(1, len(temp_arr[0]) - 1)
-    T = temp_arr[1][1]
     for i in rows:
         new_row = []
         for j in cols:
             T = (kappa / h ** 2) * (
             temp_arr[i + 1][j] - 4 * temp_arr[i][j] + temp_arr[i - 1][j] + temp_arr[i][j + 1] + temp_arr[i][
-                j - 1])  # Spreading
-            T -= k * temp_arr[i][j]  # Cooling
-            T += delta * R(temp_arr[i][j], fuel_arr[i-1][j-1])  # Combustion
+                j - 1])  # Thermal diffusion
+            T += cooling(temp_arr[i][j], ambientTemp, k)  # Cooling
+            T += delta * R(temp_arr[i][j], fuel_arr[i - 1][j - 1])  # Combustion
             new_row.append(T)
         new_arr.append(new_row)
     return np.array(new_arr)
@@ -52,6 +65,7 @@ def dTdt(temp_arr, fuel_arr, kappa, k, delta, h=1):
 # dVdt - differential equation for fuel (V) in a cell
 # arr - np.array - array of fuel values
 # beta - float - constant
+# returns - np.array - 2D array of derivatives
 def dVdt(temp_arr, fuel_arr, beta):
     new_arr = []
     for i in range(len(fuel_arr)):
@@ -64,15 +78,21 @@ def dVdt(temp_arr, fuel_arr, beta):
 
 # calcTemp - calculates the current temperature using the
 # previous temperature array and the derivative
-# arr - np.array - 2D temperature array
+# temp_arr - np.array - 2D temperature array
+# fuel_arr - np.array - 2D fuel array
 # timestep - float - time interval between steps
 # kappa - float - thermal diffusivity
-def calcTemp(temp_arr, fuel_arr, timestep, kappa, k, delta, der_func=dTdt, h=1):
-    return temp_arr + timestep * dTdt(temp_arr, fuel_arr, kappa, k, delta)
+# returns - np.array - updated array of temperatures
+def calcTemp(temp_arr, fuel_arr, timestep, kappa, k, delta, ambientTemp, der_func=dTdt, h=1):
+    return temp_arr + timestep * dTdt(temp_arr, fuel_arr, kappa, k, delta, ambientTemp)
 
 
 # calcFuel - calculates the current amount of fuel in a cell using
 # the previous temperature array and the derivative
+# temp_arr - np.array - 2D temperature array
+# fuel_arr - np.array - 2D fuel array
+# timestep - float - time interval between steps
+# beta - float - constant
+# returns - np.array - updated array of temperatures
 def calcFuel(temp_arr, fuel_arr, timestep, beta):
     return fuel_arr + timestep * dVdt(temp_arr, fuel_arr, beta)
-
